57d97c9bff52bbbba5e8ef33f26d38a233783219
Sivaharsha <sivaharsha.sottu@gmail.com>
2026-09-09 21:08:06 +0530

    Add solution: 50. Pow(x, n) (python3, v1)

---
+++ medium/50-powx-n/python3_v1.py
@@ -0,0 +1,8 @@
+# Pushed: 2026-09-09 15:38:06 UTC
+# Difficulty: Medium
+# Runtime: 3 ms
+# Memory: 19.5 MB
+
+class Solution:
+    def myPow(self, x: float, n: int) -> float:
+        return x ** n
